ab3936db9f9c4abc6cd5393465164785ea7cb2e0
Anton Chirikalov <anton.chirikalov@opsramps.com>
2022-04-27 17:38:00 +0300

    revert commit

--- operator/builtin/cachedpersister/persister.go
+++ operator/builtin/cachedpersister/persister.go
@@ -2,11 +2,8 @@ package cachedpersister
 
 import (
 	"bytes"
-	"context"
 	"encoding/json"
-	"fmt"
 	"github.com/opsramp/stanza/operator/helper"
-	"sync"
 	"time"
 
 	"github.com/opsramp/stanza/database"
@@ -15,8 +12,6 @@ import (
 
 type CachedBoltPersister struct {
 	db               database.Database
-	cache            map[string][]byte
-	mux              sync.RWMutex
 	scope            string
 	flushingInterval time.Duration
 }
@@ -24,90 +19,9 @@ type CachedBoltPersister struct {
 func NewPersister(db database.Database, scope string, flushingInterval helper.Duration) *CachedBoltPersister {
 	return &CachedBoltPersister{
 		db:               db,
-		cache:            make(map[string][]byte),
 		scope:            scope,
 		flushingInterval: flushingInterval.Duration,
 	}
-}
-
-func (b *CachedBoltPersister) StartFlusher(ctx context.Context) {
-	ticker := time.NewTicker(b.flushingInterval)
-	defer ticker.Stop()
-	for {
-		select {
-		case <-ctx.Done():
-			b.Flush()
-			b.db.Close()
-			return
-		case <-ticker.C:
-			b.Flush()
-		}
-	}
-
-}
-
-func (b *CachedBoltPersister) LoadAll() error {
-	return b.db.View(func(tx *bbolt.Tx) error {
-		bucket := tx.Bucket([]byte(b.scope))
-
-		if bucket == nil {
-			return fmt.Errorf("bucket missing for scope %s", b.scope)
-		}
-
-		c := bucket.Cursor()
-		b.mux.Lock()
-		defer b.mux.Unlock()
-		for k, v := c.First(); k != nil; k, v = c.Next() {
-			b.cache[string(k)] = v
-		}
-
-		return nil
-	})
-}
-
-func (b *CachedBoltPersister) Flush() error {
-	return b.db.Update(func(tx *bbolt.Tx) error {
-		bucket, err := tx.CreateBucketIfNotExists([]byte(b.scope))
-		if err != nil {
-			return err
-		}
-
-		b.mux.RLock()
-		defer b.mux.RUnlock()
-		for k, v := range b.cache {
-			id := new(FileIdentifier)
-			dec := json.NewDecoder(bytes.NewReader(v))
-			if err := dec.Decode(&id); err != nil {
-				print(err)
-			}
-
-			err := bucket.Put([]byte(k), v)
-			if err != nil {
-				return err
-			}
-		}
-
-		return nil
-	})
-}
-
-func (b *CachedBoltPersister) Clear() error {
-	return b.db.Update(func(tx *bbolt.Tx) error {
-		bucket := tx.Bucket([]byte(b.scope))
-		err := bucket.DeleteBucket([]byte(b.scope))
-		if err != nil {
-			return err
-		}
-		_, err = tx.CreateBucket([]byte(b.scope))
-		return err
-	})
-
-}
-
-func (b *CachedBoltPersister) ClearCache() {
-	b.mux.Lock()
-	defer b.mux.Unlock()
-	b.cache = make(map[string][]byte)
 }
 
 type FileIdentifier struct {
@@ -119,20 +33,33 @@ type Fingerprint struct {
 	FirstBytes []byte
 }
 
-func (b *CachedBoltPersister) Put(key string, value []byte) {
-	b.mux.Lock()
-	defer b.mux.Unlock()
-	b.cache[key] = value
+func (b *CachedBoltPersister) Put(key string, value []byte) error {
+
+	return b.db.Update(func(tx *bbolt.Tx) error {
+		bucket, err := tx.CreateBucketIfNotExists([]byte(b.scope))
+		if err != nil {
+			return err
+		}
+		id := new(FileIdentifier)
+		dec := json.NewDecoder(bytes.NewReader(value))
+		if err := dec.Decode(&id); err != nil {
+			return err
+		}
+
+		return bucket.Put([]byte(key), value)
+	})
+
 }
 
-func (b *CachedBoltPersister) Get(key string) ([]byte, bool) {
-	b.mux.RLock()
-	defer b.mux.RUnlock()
-	id, ok := b.cache[key]
-
-	return id, ok
-}
-
-func (b *CachedBoltPersister) IsEmpty() bool {
-	return len(b.cache) == 0
+func (b *CachedBoltPersister) Get(key string) []byte {
+	var value []byte
+	b.db.View(func(tx *bbolt.Tx) error {
+		bucket := tx.Bucket([]byte(b.scope))
+		if bucket == nil {
+			return nil
+		}
+		value = bucket.Get([]byte(key))
+		return nil
+	})
+	return value
 }
